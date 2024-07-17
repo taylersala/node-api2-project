84c0b45b436d2b22dc08e94bab2fabef928c7d8a
taylersala <taylersala@gmail.com>
2024-07-17 14:41:34 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,19 +1,59 @@
 // implement your posts router here
 const express = require('express');
+const Post = require('./posts-model');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).send('hello from the /get/api/posts endoiunt')
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
 });
 
-router.get('./:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const id = req.params.id
-    res.status(200).send(`hello from the GET /api/posts/${id} endpoint`);
+    try {   
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 });
 
 router.post('/', (req, res) => {
     res.status(200).send(`hello from the POST /posts/${id}`);
 });
 
+router.delete('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.get('/:id/messages', (req, res) => {
+
+});
+
+
 module.exports = router;
+
--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server');
 const port = 9000;
 
 server.listen(port, () => {
-    console.log(`server is listening on http://localhost:{port}`);
+    console.log(`server is listening on http://localhost:${port}`);
 });
