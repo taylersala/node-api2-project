ed9c7e2325437da6dcc23f622067002a6936d83c
taylersala <taylersala@gmail.com>
2024-07-17 13:57:39 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,19 @@
 // implement your posts router here
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).send('hello from the /get/api/posts endoiunt')
+});
+
+router.get('./:id', (req, res) => {
+    const id = req.params.id
+    res.status(200).send(`hello from the GET /api/posts/${id} endpoint`);
+});
+
+router.post('/', (req, res) => {
+    res.status(200).send(`hello from the POST /posts/${id}`);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+ server.get('/', (req, res) => {
+    res.send('api up and running')
+});
+
+module.exports = server; 
